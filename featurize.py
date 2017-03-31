5ebb35035a910f1754a88c819eca057f128919f0
clayriley <riley@brandeis.edu>
2017-03-30 23:47:29 -0400

    bug repaired in which reference clusters were matched incorrectly

--- featurize.py
+++ featurize.py
@@ -16,6 +16,9 @@ class Featurizer:
         
         # record whether they corefer, if training
         coreference = int(antecedent[0][-1]==anaphor[0][-1]) if training else None
+        refs_i = set(antecedent[0][-1])
+        refs_j = set(anaphor[0][-1])
+        coreference = int(refs_i.isdisjoint(refs_j)) if training else None
         
         # generate values for use in features
         sentence_dist = anaphor[0][0] - antecedent[-1][0]
@@ -83,7 +86,9 @@ class Featurizer:
                             refID += 1
                     
                     # add this token's info to all opened entities
+                    fields[-1] = []
                     for ent in open_entities:
+                        fields[-1].append(ent)
                         open_entities[ent].append(fields)
                     
                     # current token has entity end(s), close it
